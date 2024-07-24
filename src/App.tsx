b535a0161a3a3fe786249fd4ed65110aa427e1b2
haslan82 <haslan82@hotmail.com>
2024-07-24 23:55:53 +0300

    ekledim

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,24 @@
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header";
 import Hero from "./components/Hero";
+import SearchBar from "./components/SearchBar";
+import Filter from "./components/Filter";
+import { useEffect, useState } from "react";
+import fetchCars from "./utils/fetchCars";
+import { CarType } from "./utils/types";
 
 
 const App = () => {
+const [cars, setCars] = useState<CarType[] | null>(null);
+const [isError, setIsError] = useState<boolean>(false);
+
+  useEffect (() => {
+    fetchCars()
+    .then((data)=> setCars(data))
+    .catch(() => setIsError(true));
+  }, []);
+
+  console.log(isError, cars)
 
   return (
     <div className="min-h-screen text-white bg-[rgb(23,23,23)] ">
@@ -11,6 +26,19 @@ const App = () => {
       <BrowserRouter>
         <Header />
         <Hero />
+        <div className="mt-12 padding-x padding-y max-width">
+          <div className="home__text-container">
+            <h1 className="text-4xl font-extrabold ">Araba Katoloğu</h1>
+            <p>Beğenebileceğin arabaları keşfet</p>
+            <div className="home__filters">
+              <SearchBar />
+              <div className="home__filter-container">
+                <Filter/>
+                <Filter/>
+              </div>
+            </div>
+          </div>
+        </div>
       </BrowserRouter>
 
     </div>
---
+++ src/utils/fetchCars.tsx
@@ -0,0 +1,27 @@
+import { CarType } from "./types";
+
+const options = {
+	method: 'GET',
+	headers: {
+		/* 'x-rapidapi-key': '4881877b7fmsh063d77fae5a42d9p1be6e9jsn8d4ae58e7971', */
+        "x-rapidapi-key": "75dc092df0msh3c03138e5cc1ea2p19035ejsn916bcc592247",
+		'x-rapidapi-host': 'cars-by-api-ninjas.p.rapidapi.com'
+	}
+};
+
+const fetchCars =async ():Promise<CarType[]>=> {
+
+    const url = 'https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=m5';
+
+const res = await fetch(url, options);
+
+const data = await res.json();
+
+//console.log(data);
+return data;
+
+};
+
+
+
+export default fetchCars;
